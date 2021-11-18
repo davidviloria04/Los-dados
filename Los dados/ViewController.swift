563067c55f4c36998b44d63758d0d85edb6f0d0a
David Viloria <david.viloria0424@gmail.com>
2021-11-17 22:18:57 -0500

    Implementar numero ramdom
    Utiliza la función arc4Random

--- Los dados/ViewController.swift
+++ Los dados/ViewController.swift
@@ -12,14 +12,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceLeft: UIImageView!
     @IBOutlet weak var diceRight: UIImageView!
     
+    var randomDiceIndexLeft: Int = 0
+    var randomDiceIndexRight: Int = 0
+    
+    
+    let diceImages: [String] = ["Dices1" , "Dices2", "Dices3", "Dices4",  "Dices5", "Dices6"]
+    let nFaces: UInt32
+    
+    required init?(coder aDecoder: NSCoder) {
+        nFaces = UInt32(diceImages.count)
+        super.init(coder: aDecoder)
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        generateRamdomDices()
     }
     
-    @IBAction func rollPreset(_ sender: UIButton) {
+    @IBAction func rollPresedd(_ sender: UIButton) {
+        generateRamdomDices()
+    }
+    
+    func generateRamdomDices(){
         
+        randomDiceIndexLeft = Int(arc4random_uniform(nFaces)) //Genera aleatoriamente el dado izquierdo
+        randomDiceIndexRight = Int(arc4random_uniform(nFaces)) //Genera aleatoriamente el dado derecho
+        
+        diceLeft.image = UIImage(named: diceImages[randomDiceIndexLeft]) //Cambia la imagen del lado izquierdo
+        diceRight.image = UIImage(named: diceImages[randomDiceIndexRight]) //Cambia la imagen del lado derecho
+        
+        
+        UIView.animate(withDuration: 0.8, delay: 0.2, options: UIView.AnimationOptions.curveEaseIn) {
+            <#code#>
+        } completion: { <#Bool#> in
+            <#code#>
+        }
+
     }
 }
 
